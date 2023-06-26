2fc77f8432c97a96ca017fd932a7758313e8d939
Danjel Peqini <106821739+danjel-peqini@users.noreply.github.com>
2023-06-26 02:08:57 +0200

    Add files via upload

---
+++ puppeterr.js
@@ -0,0 +1,45 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+        args:['--start-maximized' ]
+
+    });
+      const page = await browser.newPage();
+  await page.goto('https://fpp-ui.3i-solutions.net/dashboard');
+    // await page.click("button", {delay: 2000})
+    setTimeout(async() => {
+        
+        await page.type("input", "kandidat", {delay: 200})
+        await page.keyboard.press('Tab')
+        await page.type("p-password", "asdasdasd", {delay: 200})
+        await page.keyboard.press("Enter", {delay: 2000})
+        await page.waitForNavigation()
+        await page.click("button", {delay: 3000})
+        let options = [];
+       
+    const dropdown = await page.$("p-dropdown")
+    await dropdown.click()
+    const option = await dropdown.$("li")
+     await option?.click()
+     const button =  await page.waitForSelector('button[label="Krijo Raportin"]', {
+      visible: true,
+      disable: false
+    })
+    await button.click()
+    
+        // await page.click("[label=Krijo Raportin]", {delay: 600})
+        // await dropdown.select()
+    }, 5000);
+  // Set screen size
+//   await page.setViewport({width: 1080, height: 1024});
+
+  // Type into search box
+
+  // Wait and click on first result
+
+//   await browser.close();           
+})();
