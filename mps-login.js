24c6e60f447b3d0a938ed6dd91f5c26d4c23a98c
Danjel Peqini <danjel.peqini@3i-solutions.net>
2023-08-22 15:00:54 +0200

    remote

--- mps-login.js
+++ mps-login.js
@@ -7,7 +7,7 @@ const request_client = require('request-promise-native');
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
-        executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+        // executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
         args: ['--start-maximized']
 
     });
